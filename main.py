9f546d46c58e399220416e6a89a2656270fdf7a7
GabroVV <origingabriel3124@gmail.com>
2020-04-30 00:06:17 +0200

    Ostry gaz

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import file_ops as fo
+import pandas as pd
 
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
+import seaborn as sns
 from tensorflow import keras
 from tensorflow.keras import layers
-
 import tensorflow_docs as tfdocs
 import tensorflow_docs.plots
 import tensorflow_docs.modeling
 
 
-def norm(x):
-    return (x - train_stats['mean']) / train_stats['std']
+# def norm(x):
+#     return (x - train_stats['mean']) / train_stats['std']
 
 
 def build_model():
     _model = keras.Sequential([
         layers.Dense(64, activation='relu', input_shape=[len(train_dataset.keys())]),
-        layers.Dense(64, activation='relu'),
-        layers.Dense(1)
+        layers.Dense(512, activation='relu'),
+        layers.Dense(512, activation='relu'),
+        layers.Dense(2)
     ])
 
     optimizer = tf.keras.optimizers.RMSprop(0.001)
@@ -31,37 +33,47 @@ def build_model():
 
 
 if __name__ == '__main__':
+    final_test_data = fo.xlsx_to_dataset('files/pozyxAPI_only_localization_dane_testowe_i_dystrybuanta.xlsx')
+    final_test_data_ref = final_test_data.drop(columns=['0/timestamp','t','no','measurement x','measurement y',
+                                                        'błąd pomiaru', 'liczba błędnych próbek', '% błędnych próbek',
+                                                        'błąd', 'Unnamed: 9'])
+    final_test_data_ref = final_test_data_ref.dropna()
+
+    final_test_data_measure = final_test_data.drop(columns=['0/timestamp','t','no','reference x','reference y',
+                                                        'błąd pomiaru', 'liczba błędnych próbek', '% błędnych próbek',
+                                                        'błąd', 'Unnamed: 9'])
+    final_test_data_ref.dropna()
+    print(final_test_data_ref)
+
     dataset = fo.xlsx_to_dataset('files/pozyxAPI_dane_pomiarowe/pozyxAPI_only_localization_measurement1.xlsx')
-    print(dataset)
+    dataset.pop('0/timestamp')
+    dataset.pop('t')
+    dataset.pop('no')
 
     train_dataset = dataset.sample(frac=0.8, random_state=0)
     test_dataset = dataset.drop(train_dataset.index)
 
-    train_stats = train_dataset.describe()
-    train_stats.pop('reference x')
-    train_stats.pop('reference y')
-    train_stats = train_stats.transpose()
-    print(train_stats)
 
-    train_labels = train_dataset.pop('reference x')
-    train_dataset.pop('reference y')
-    test_labels = test_dataset.pop('reference x')
-    test_dataset.pop('reference y')
 
-    normed_train_data = norm(train_dataset)
-    normed_test_data = norm(test_dataset)
+    train_labels = train_dataset.drop(columns=['measurement x', 'measurement y'])
+
+    test_labels = test_dataset.drop(columns=["measurement x", "measurement y"])
+
+
+    train_dataset = train_dataset.drop(columns=["reference x", "reference y"])
+    test_dataset = test_dataset.drop(columns=["reference x", "reference y"])
+
+    # sns.set()
+    # sns.relplot(x="measurement x", y="measurement y",
+    #             data=train_dataset)
+    # sns.relplot(x="measurement x", y="measurement y",
+    #             data=test_dataset)
+    # plt.show()
 
     model = build_model()
-    print(model.summary())
-
-    example_batch = normed_train_data[:10]
-    example_result = model.predict(example_batch)
-    print(example_result)
-
     EPOCHS = 1000
-
     history = model.fit(
-        normed_train_data, train_labels,
+        train_dataset, train_labels,
         epochs=EPOCHS, validation_split=0.2, verbose=0,
         callbacks=[tfdocs.modeling.EpochDots()])
 
@@ -69,8 +81,21 @@ if __name__ == '__main__':
     hist['epoch'] = history.epoch
     print(hist.tail())
 
-    plotter = tfdocs.plots.HistoryPlotter(smoothing_std=2)
-    plotter.plot({'Basic': history}, metric="mse")
-    plt.ylabel('MAE')
-    # for feat, targ in dataset.take(5):
-    #     print('Features: {}, Target: {}'.format(feat, targ))
+    print(history)
+
+    test_predictions = model.predict(test_dataset)
+
+    test_predictions_dataframe = pd.DataFrame(test_predictions, columns=['x', 'y'])
+
+
+    final_test_data_predictions = model.predict(final_test_data_measure)
+    final_test_data_predictions_dataframe = pd.DataFrame(final_test_data_predictions, columns=['x', 'y'])
+
+    sns.relplot(x="x", y="y",
+                data= final_test_data_predictions_dataframe)
+    sns.relplot(x="reference x", y="reference y",
+                data=final_test_data_ref)
+    sns.relplot(x="measurement x", y="measurement y",
+                data=final_test_data_measure)
+    plt.show()
+
